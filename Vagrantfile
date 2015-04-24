1b0228a58a7cd422c0a00ba43dfcdfedb47abbf7
NLZ13 <verbeekniels13@gmail.com>
2015-04-24 14:17:35 +0200

    phpmyadmin install

--- Vagrantfile
+++ Vagrantfile
@@ -324,6 +324,9 @@ Vagrant.configure("2") do |config|
   # Install Android
   # config.vm.provision "shell", path: "#{github_url}/scripts/android.sh"
 
+  # Install Android
+  config.vm.provision "shell", path: "#{github_url}/scripts/phpmyadmin.sh"
+
   ####
   # Local Scripts
   # Any local scripts you may want to run post-provisioning.
---
+++ scripts/phpmyadmin.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+echo ">>> Installing PhpMyAdmin"
+
+URL=http://switch.dl.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.4.3/phpMyAdmin-4.4.3-all-languages.tar.bz2
+FILE=${URL##*/}
+TARGET=/opt/phpmyadmin
+
+[ ! -f /tmp/${FILE} ] && wget -nv ${URL} -O /tmp/${FILE}
+
+rm -Rf ${TARGET}/phpMyAdmin-*/
+
+mkdir $TARGET
+tar -xjf /tmp/${FILE} -C ${TARGET}
+
+for dir in `ls /opt/phpmyadmin`;
+do
+    rm /vagrant/phpmyadmin
+    ln -s ${TARGET}/$dir/ /vagrant/phpmyadmin
+done
+
+cat <<< '<?php
+$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$i = 0;
+$i++;
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = '';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+' > /vagrant/phpmyadmin/config.inc.php
